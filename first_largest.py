8cf2fad982fd86bd2c7a8a2cd1e99fc863b634eb
JMG <jmgrote215@ksu.edu>
2021-09-22 12:32:05 -0500

    changes to the unit testing. not correct. see vidio

--- first_largest.py
+++ first_largest.py
@@ -2,12 +2,24 @@
 Python program to find the largest element and its location.
 """
 
-def largest_element(a):
+def largest_element(a,loc):
     """ Return the largest element of a sequence a.
     """
 
-    return None
+    maxval = a[0]
+    for i in range(1, len(a)):
+        if a[i] > maxval:
+            maxval = a[i]
+            indicii = i
 
+
+    large = sorted(a)
+    index = len(a)
+    if loc == True:
+    #return large[index-1]
+        return maxval,indicii
+    else:
+        return maxval
 
 if __name__ == "__main__":
 
--- tests.py
+++ tests.py
@@ -5,12 +5,20 @@ from first_largest import largest_element
 class FirstLargest(unittest.TestCase):
 
     def test_largest_is_first(self):
-        given = [1,2,3,2,1]
-        expect = 3
+        #given = [1,2,3,2,1]
+        given = [5,3,4,1,2]
+        expect = 5
         got = largest_element(given)
         self.assertEqual(got, expect)
    
     """ ADD MORE  """
+    def test_largest_is_first_with_location(self):
+
+        given = [5,3,4,1,2]
+        expect = 5,0
+        got = largest_element(given, loc=True)
+        self.assertEqual(got, expect)
+
 
 if __name__ == '__main__':
     unittest.main()
